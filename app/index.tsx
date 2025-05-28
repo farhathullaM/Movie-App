e11dac6a9ad44b04dfd538af3ad2108e48db5dcc
farhathulla <farhathulla01@gmail.com>
2025-05-28 10:34:25 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center">
+      <Text className="text-3xl bg-red-500 w-full text-center font-bold text-light-200">Welcome</Text>
+    </View>
+  );
+}
