ea7627bbfe16a309e187cbb3246d9151e379c75d
farhathulla <farhathulla01@gmail.com>
2025-05-29 12:06:00 +0530

    tab

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='flex items-center justify-center h-full'>
+      <Text>index is okke</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
