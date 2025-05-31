709941600f3111b21fbbeb15f5b47858976832a2
farhathulla <farhathulla01@gmail.com>
2025-05-31 23:21:53 +0530

    tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,90 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
+import { Image, ImageBackground, Text, View } from "react-native";
+
+const TabIcon = ({ focused, icon, text }: any) => {
+  if (focused)
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px]  justify-center mt-4 min-h-16 items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold">{text}</Text>
+      </ImageBackground>
+    );
+
+  return (
+    <View className="size-full justify-center items-center mt-5">
+      <Image source={icon} className="size-5 " tintColor={"#a8b5db"} />
+    </View>
+  );
+};
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          height: 52,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "0f0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} text="Home" />
+          ),
+        }}
       />
       <Tabs.Screen
         name="search"
-        options={{ title: "Search", headerShown: false }}
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} text="Search" />
+          ),
+        }}
       />
       <Tabs.Screen
         name="saved"
-        options={{ title: "Saved", headerShown: false }}
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} text="Saved" />
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
-        options={{ title: "Profile", headerShown: false }}
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} text="Profile" />
+          ),
+        }}
       />
     </Tabs>
   );
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
-const index = () => {
+export default function Index() {
   return (
-    <View className='flex items-center justify-center h-full'>
-      <Text>index is okke</Text>
+    <View className="flex items-center justify-center h-full">
+      <Text className="text-3xl bg-red-500 w-full text-center font-bold text-light-200">
+        Welcome
+      </Text>
+      <Link
+        href="/onboarding"
+        className="text-3xl w-full text-center font-bold text-light-200"
+      >
+        Onboarding
+      </Link>
     </View>
-  )
+  );
 }
-
-export default index
-
-const styles = StyleSheet.create({})
